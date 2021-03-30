a922724750d00227ed9038201f66fda608f3049c
tborghetti <tborghetti@udesa.edu.ar>
2021-03-30 16:14:00 -0300

    Componentes

--- src/App.js
+++ src/App.js
@@ -1,6 +1,14 @@
+import Footer from './components/Footer';
+// import Header from './components/Header';
+// import Main from './components/Main';
+
 function App() {
   return (
-  <p>Holi</p>
+  <div> 
+  {/* <Header /> */}
+  {/* <Main /> */}
+  <Footer />
+  </div>
   );
 }
 
---
+++ src/components/Cards.js
@@ -0,0 +1,8 @@
+function Cards (props){
+    return (
+    <div className='cards'>
+    <p> holi </p>
+    </div>
+    );
+    }
+    export default Cards;
