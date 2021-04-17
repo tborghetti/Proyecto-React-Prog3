e3468a1a52cbba2492675a117c9247a53b8b4d7b
arielfarji <afarji@udesa.edu.ar>
2021-04-17 13:08:36 -0300

    API, visualizacion de tarjeta, detalle de contacto y borrar
    Todo el equipo en conjunto: completamos punto 1, 2 y del punto 3 terminamos ver detalle y borrar tajeta.

--- src/components/Card.js
+++ src/components/Card.js
@@ -1,22 +1,76 @@
-import React from 'react';
+import React, { Component } from 'react';
 
-function Card(props) {
-    return (
-        <div className="Card uk-grid-small uk-background-muted uk-padding uk-card-hover uk-width-1-3@m">
-            <div className="uk-grid-small uk-flex-middle" uk-grid>
-                <p className="other" >{props.DataShown.gender}</p>
-                <div className="uk-width-auto">
-                    <img src={props.DataShown.picture.medium} alt=""/>
-                    <h2 className="name uk-card-title uk-margin-remove-bottom"> {props.DataShown.name.last}, {props.DataShown.name.first}</h2>
-                    <p className="other uk-text-meta uk-margin-remove-top">{props.DataShown.email}</p>
+export default class Card extends Component {
+    constructor(props) {
+        super(props);
+        this.state = {
+
+        }
+    }
+
+    render() {
+        return (
+            <div className="Card uk-grid-small uk-background-muted uk-padding uk-card-hover uk-width-1-3@m">
+                <a onClick={this.props.onDelete.bind(this, this.props.id)} href= "#"><i class="fas fa-times"></i></a>
+                <div className="uk-grid-small uk-flex-middle" uk-grid>
+                    <p className="other" >{this.props.DataShown.gender}</p>
+                    <div className="uk-width-auto">
+                        <img src={this.props.DataShown.picture.medium} alt="" />
+                        <h2 className="name uk-card-title uk-margin-remove-bottom"> {this.props.DataShown.name.last}, {this.props.DataShown.name.first}</h2>
+                        <p className="other uk-text-meta uk-margin-remove-top">{this.props.DataShown.email}</p>
+                        <hr></hr>
+                    </div>
+                    <p className="other" >{this.props.DataShown.dob.date} - ({this.props.DataShown.dob.age}) </p>
                     <hr></hr>
-                </div> 
-                <p className="other" >{props.DataShown.dob.date} - {props.DataShown.dob.age} </p>
-            <hr></hr>
-            <button className="uk-button uk-button-danger">Borrar</button>
+                </div>
+                <ul uk-accordion="multiple: true">
+                    <li className="uk-close">
+                        <a className="uk-accordion-title" href="#">More info</a>
+                        <div className="uk-accordion-content">
+                            <ul>
+                                <li>
+                                    <p>
+                                        Calle y numero: 
+                                       {this.props.DataShown.location.street.name} {this.props.DataShown.location.street.number}
+                                    </p>
+                                </li>
+                                <li>
+                                    <p>
+                                        Ciudad - estado:
+                                       {this.props.DataShown.location.city} - {this.props.DataShown.location.state}
+                                    </p>
+                                </li>
+                                <li>
+                                    <p>
+                                        País:
+                                       {this.props.DataShown.location.country}
+                                    </p>
+                                </li>
+                                <li>
+                                    <p>
+                                        Codigo Postal:
+                                       {this.props.DataShown.location.postcode}
+                                    </p>
+                                </li>
+                                <li>
+                                    <p>
+                                        Fecha de registro:
+                                        {this.props.DataShown.registered.date}
+                                    </p>
+                                </li>
+                                <li>
+                                    <p>
+                                        Telefono:
+                                        {this.props.DataShown.phone}
+                                    </p>
+                                </li>
+                            </ul>
+                        </div>
+                    </li>
+                </ul>
             </div>
-        </div>
-        
-    );
+
+        );
+    }
 }
-export default Card;
+
--- src/components/CardsContainer.js
+++ src/components/CardsContainer.js
@@ -1,28 +1,43 @@
-import React from 'react';
+import { Component } from 'react';
 import Card from './Card';
+import React from 'react';
 
-const infoCards = [
-    {"gender": "male", "name": { "title": "Mr", "first": "Øystein", "last": "Eikenes" }, "email": "oystein.eikenes@example.com", "dob": { "date": "1951-03-20T03:37:34.443Z", "age": 70 },
-        "picture": { "large": "https://randomuser.me/api/portraits/men/85.jpg", "medium": "https://randomuser.me/api/portraits/med/men/85.jpg", "thumbnail": "https://randomuser.me/api/portraits/thumb/men/85.jpg" }, "nat": "NO"},
-    {"gender": "male", "name": { "title": "Mr", "first": "Jorge", "last": "Martinez" }, "email": "jmartinez@example.com", "dob": { "date": "1951-03-20T03:37:34.443Z", "age": 60 }, "id": { "name": "FN", "value": "20035147367" }, 
-        "picture": { "large": "https://randomuser.me/api/portraits/men/85.jpg", "medium": "https://randomuser.me/api/portraits/med/men/85.jpg", "thumbnail": "https://randomuser.me/api/portraits/thumb/men/85.jpg" }, "nat": "NO"},
-    {"gender": "female", "name": { "title": "Mrs", "first": "Maria", "last": "Fernandez" }, "email": "mariafernandez@example.com", "dob": { "date": "1951-03-20T03:37:34.443Z", "age": 50 }, "id": { "name": "FN", "value": "20035147365" }, 
-        "picture": { "large": "https://randomuser.me/api/portraits/men/85.jpg", "medium": "https://randomuser.me/api/portraits/med/men/85.jpg", "thumbnail": "https://randomuser.me/api/portraits/thumb/men/85.jpg" }, "nat": "NO"},
-    {"gender": "male", "name": { "title": "Mr", "first": "Lautaro", "last": "Gomez" }, "email": "lgomez@example.com", "dob": { "date": "1951-03-20T03:37:34.443Z", "age": 30 }, "id": { "name": "FN", "value": "20035147365" }, 
-        "picture": { "large": "https://randomuser.me/api/portraits/men/85.jpg", "medium": "https://randomuser.me/api/portraits/med/men/85.jpg", "thumbnail": "https://randomuser.me/api/portraits/thumb/men/85.jpg" }, "nat": "NO"}
-    ]
+export default class CardsContainer extends Component {
+    constructor(props){
+        super(props);
+        this.state = {
+            infoCards:[ ]
+        }
+    }
+    componentDidMount(){
+        fetch('https://randomuser.me/api/?results=100')
+        .then((result) => result.json())
+        .then((data) => { 
+          this.setState({infoCards: data.results})
+          console.log(data.results)
+        })
+      }
 
-function CardsContainer() {
-    return (
-        <div className="uk-grid-collapse uk-text-center" uk-grid='true'>
-            {infoCards.map(function (oneCard, idx) {
-                return (
-                    <Card DataShown={oneCard} key={idx} />
-                )
-            })
-            }
-        </div>
-    );
+      delete(idCard){
+        console.log(idCard);
+        let person = this.state.infoCards.filter((item)=>{
+         return item.id !== idCard
+       })
+       this.setState({infoCards: person})
+      }
+
+    render(){
+        return (
+            <div className="uk-grid-collapse uk-text-center" uk-grid='true'>
+                {this.state.infoCards.map( (oneCard, idx) => {
+                    return (
+                        <Card DataShown={oneCard} key={idx} onDelete={this.delete.bind(this)} id={oneCard.id} />
+
+                    )
+                })
+                }
+            </div>
+        );
+    }
 }
 
-export default CardsContainer;
